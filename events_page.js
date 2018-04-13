296ef831a84dedaf86879e1af3cb4832ac7ef71d
ocampossoto <ocampossoto@users.noreply.github.com>
2018-04-13 18:57:01 -0500

    Display working again

--- events_page.js
+++ events_page.js
@@ -1,17 +1,29 @@
+//firebase config
 var config = {
     apiKey: "AIzaSyB4Mpz4ECLl9RQU8kJZGg84j-EAcV7yKSA",
     authDomain: "communityconnect-3f395.firebaseapp.com",
     databaseURL: "https://communityconnect-3f395.firebaseio.com",
     storageBucket: "communityconnect-3f395.appspot.com",
 };
+//Firebase initializing
 firebase.initializeApp(config);
-
+//get database
 var database = firebase.database();
-
+//create events list for population later
 var events_list =[];
-
+//database reference
 var Ref = database.ref('Community List');
 
+
+var user = firebase.auth().currentUser;
+
+if (user) {
+    console.log("User");
+} else {
+    console.log("No User");
+}
+
+//function to get data asych from database and save to variables
 function get_events() {
     return Ref.once('value').then(function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
