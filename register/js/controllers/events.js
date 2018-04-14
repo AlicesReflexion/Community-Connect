0b90715a18e5c3be40fd26d86607779526fa1358
ocampossoto <ocampossoto@users.noreply.github.com>
2018-04-14 11:50:34 -0500

    Working event functions

--- register/js/controllers/events.js
+++ register/js/controllers/events.js
@@ -1,11 +1,18 @@
 myApp.controller('EventsController', ['$scope','$location','communityService', function($scope,$location, communityService) {
-  $scope.message = "Events";
+  $scope.EventList =[];
 
   var community_id = communityService.getCommunity_id();
   if(community_id == undefined){
       $location.path('/success');
   }
   console.log(community_id);
+  var currentPostsRef = firebase.database().ref('/Community List/' + community_id + '/Events');
+  currentPostsRef.on('value', function(snapshot) {
+      Object.values(snapshot.val()).forEach(function(element) {
+        console.log(element);
+         $scope.EventList.push(element);
+          $scope.$apply();
+      });
+  });
 
-  
 }]);
