8be6a5a5b55b82441c1b4358e38097b8c895ee8c
ocampossoto <ocampossoto@users.noreply.github.com>
2018-04-13 19:22:05 -0500

    Display working with code.

--- events_page.js
+++ events_page.js
@@ -13,39 +13,52 @@ var database = firebase.database();
 var events_list =[];
 //database reference
 var Ref = database.ref('Community List');
-
+//name of the current community
+var community = "-adfweofdkdubkx";
 
 var user = firebase.auth().currentUser;
-
-if (user) {
-    console.log("User");
-} else {
+//verify user
+if (user == null) {
+    //redirect user to sign in page
     console.log("No User");
 }
 
 //function to get data asych from database and save to variables
 function get_events() {
+    //return the promise of the database
     return Ref.once('value').then(function(snapshot) {
+        //go through every item of the community list
         snapshot.forEach(function(childSnapshot) {
-            var childData = (childSnapshot.toJSON());
-            var event_keys = Object.keys(childData.Events);
-            for(i=0; i < event_keys.length; i++){
-                var current_event = childData.Events[event_keys[i]];
-                events_list.push(current_event);
+            //check if we are at the correct community
+            if(childSnapshot.key == -adfweofdkdubkx){
+                //save the json data of that community
+                var childData = (childSnapshot.toJSON());
+                //save the keys for the event
+                var event_keys = Object.keys(childData.Events);
+                //go through events and add it to the event list (global)
+                for(i=0; i < event_keys.length; i++){
+                    //save event
+                    var current_event = childData.Events[event_keys[i]];
+                    //push it to list
+                    events_list.push(current_event);
+                }
             }
         });
+        //return the promise.
         return snapshot.val();
     });
 }
 
 angular.module('myApp', [])
-
     .controller('View1Ctrl', function ($scope) {
+        //make the event list for the page to udpate with
         $scope.EventList =[];
         get_events().then(function(successCallback, failureCallback){
             if(successCallback){
+                //since we got a result
+                //save the list to the page
                 $scope.EventList = events_list;
-
+                //update the page.
                 $scope.$apply();
             }
         });
