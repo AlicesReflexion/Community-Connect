1b07493ff7184933753bb6408d4b28b82661d714
ocampossoto <ocampossoto@users.noreply.github.com>
2018-04-15 12:36:02 -0500

    Working creation of communities

--- register/js/controllers/create.js
+++ register/js/controllers/create.js
@@ -1,4 +1,37 @@
-myApp.controller('CreateController', [function() {
-  // $scope.message ="something";
+myApp.controller('CreateController', ['$scope','$firebaseObject','$location', function($scope, $firebaseObject, $location) {
+
+    $scope.back_suc = function(){
+        try {
+            // session code here
+            sessionStorage.setItem("community_id", $scope.community)
+        }
+        catch (e) {
+            sessionStorage.clear();
+        }
+        $location.path('/success');
+    };
+
+    try{
+        $scope.userid = sessionStorage.getItem("userID");
+    }
+    catch(e){
+        $scope.back_suc();
+    }
+    if($scope.userid === null){
+        $scope.back_suc();
+    }
+    $scope.create_community = function() {
+        if($scope.userid !== null){
+            //console.log("Creatng!", $scope.Community.Community_Desciption);
+            firebase.database().ref('Community List/').push({
+                Name: $scope.Community.Name,
+                Description: $scope.Community.Community_Desciption,
+                Admin: [$scope.userid],
+                Members: [$scope.userid]
+            });
+            $scope.back_suc();
+        }
+    }
+
 
 }]);
--- register/js/controllers/success.js
+++ register/js/controllers/success.js
@@ -6,48 +6,71 @@ myApp.controller('SuccessController', ['$rootScope','$scope', '$location', '$fir
 
     $scope.PriorityList=[];
     $scope.PostList =[];
-    $scope.current_user =null;
-    $scope.community=null;
-    $scope.community = sessionStorage.getItem("community_id");
-    $scope.userid = sessionStorage.getItem("userID");
+    $scope.current_user = "null";
+
+    try{
+        $scope.community = sessionStorage.getItem("community_id");
+        console.log($scope.community);
+    }
+    catch(e){
+        $scope.community = "null";
+    }
+
+    try{
+        $scope.userid = sessionStorage.getItem("userID");
+        console.log($scope.userid);
+    }
+    catch(e){
+        $scope.userid = "null";
+    }
 
 
     //var user = $rootScope.currentUser;
   $scope.go_events_page = function(){
-      $scope.save_user_community();
+      $scope.save_user_data();
       $location.path('/events');
 
   };
 
+    $scope.new_community = function(){
+        $scope.save_user_data();
+        $location.path('/community_create');
+    }
+
    //get community
     $scope.getCommunity = function(){
+        console.log("Started", $scope.community == null);
         //go through database and see if we are apart of the community
         var x = firebase.database().ref('/Community List/');
         x.once('value').then(function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 //check if we found the community we are in
-                if($scope.community == null) {
+                if($scope.community === "null" || $scope.community == null) {
                     childSnapshot.forEach(function (item) {
                         //check if we are in the members section
-                        if (item.key == "Members") {
+                        if (item.key === "Members") {
                             //go through list and compare ID's
                             for (i = 0; i < item.val().length; i++) {
-                                if (item.val()[i] == $scope.current_user.uid) {
+                                if (item.val()[i] === $scope.userid) {
                                     //update community key in scope and update the scope
                                     $scope.community = childSnapshot.key;
                                     $scope.$apply();
+                                    break;
                                 }
                             }
                         }
                     });
                 }
                 else{
+                    console.log("Running Get Post");
                     //run populate posts
                     $scope.load_post();
                 }
             });
-            if($scope.community == null){
-                $location.path('/community_create');
+            if($scope.community === "null" || $scope.community == null){
+                console.log("redirecting");
+                $scope.new_community();
+                $scope.$apply();
             }
         });
 
@@ -56,43 +79,62 @@ myApp.controller('SuccessController', ['$rootScope','$scope', '$location', '$fir
     //post function
     $scope.load_post = function(){
         //go through database with the community name and get all post
-        var refCurrentPostsRef = firebase.database().ref('/Community List/' + $scope.community + '/Post');
-        refCurrentPostsRef.on('value', function(snapshot) {
-            //put post in appropriate list and update page
-            Object.values(snapshot.val()).forEach(function(element) {
-                if (element.Priority) {
-                    $scope.PriorityList.push(element);
-                } else {
-                    $scope.PostList.push(element);
+        if($scope.community == null){
+            //console.log($scope.community);
+            return;
+        }
+        else{
+            var refCurrentPostsRef = firebase.database().ref('/Community List/' + $scope.community + '/Post');
+            refCurrentPostsRef.once('value').then(function(snapshot) {
+                //put post in appropriate list and update page
+                console.log(snapshot);
+                try{
+                    Object.values(snapshot.val()).forEach(function(element) {
+                        if (element.Priority) {
+                            $scope.PriorityList.push(element);
+                        } else {
+                            $scope.PostList.push(element);
+                        }
+                        $scope.$apply();
+                    });
                 }
-                $scope.$apply();
-            });
+                catch(e){
+                    console.log("No Post");
+                }
 
-        });
+
+            });
+        }
+
     };
-    $scope.save_user_community = function(){
+    $scope.save_user_data = function(){
         try {
             // session code here
             sessionStorage.setItem("userID", $scope.current_user.uid);
             sessionStorage.setItem("community_id", $scope.community)
         }
         catch (e) {
-            alert("some errror");
+            sessionStorage.clear();
         }
     };
 
-    if($scope.userid != null){
+    if($scope.userid !== "null" && $scope.userid !== null){
         //run get community function
+        console.log("getting community", $scope.userid);
         $scope.getCommunity();
     }
     else{
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                 $scope.current_user = user;
+                $scope.userid = user.uid;
                 $scope.$apply();
+                console.log("Running get Community", $scope.userid);
+                $scope.getCommunity();
 
             } else {
                 // No user is signed in.
+                console.log("waiting for user...")
             }
         });
     }
